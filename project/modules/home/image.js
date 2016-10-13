6709fbb0c2d3800c0a8acb1a8138cfb2dc44ff34
fov42550564 <42550564@qq.com>
2016-10-14 00:14:14 +0800

    完成显示图片

--- project/modules/home/image.js
+++ project/modules/home/image.js
@@ -1,5 +1,43 @@
 var app = getApp();
 Page({
     data: {
+        list: [
+            {
+                author: '我被苍井空包养了',
+                avatar: 'http://wx.qlogo.cn/mmhead/Q3auHgzwzM76hwa8RNfuXy20DdryLojOIH46m7JIYfF8fZjIQouOpA/132',
+                content: '在这个世界上，人外有人，天外有天，不要以为自己有点本事，就目中无人，我们这里有一私人学校，舞蹈老师居然把体育老师的腿给打折了',
+                image: 'http://localhost:3000/images/1.jpg',
+                likes: 1431,
+                comments: 16,
+                shares: 4
+            },
+            {
+                author: '我被苍井空包养了',
+                avatar: 'http://wx.qlogo.cn/mmhead/Q3auHgzwzM76hwa8RNfuXy20DdryLojOIH46m7JIYfF8fZjIQouOpA/132',
+                content: '在这个世界上，人外有人，天外有天，不要以为自己有点本事，就目中无人，我们这里有一私人学校，舞蹈老师居然把体育老师的腿给打折了',
+                image: 'http://localhost:3000/images/1.jpg',
+                likes: 1431,
+                comments: 16,
+                shares: 4
+            },
+            {
+                author: '我被苍井空包养了',
+                avatar: 'http://wx.qlogo.cn/mmhead/Q3auHgzwzM76hwa8RNfuXy20DdryLojOIH46m7JIYfF8fZjIQouOpA/132',
+                content: '在这个世界上，人外有人，天外有天，不要以为自己有点本事，就目中无人，我们这里有一私人学校，舞蹈老师居然把体育老师的腿给打折了',
+                image: 'http://localhost:3000/images/1.jpg',
+                likes: 1431,
+                comments: 16,
+                shares: 4
+            },
+            {
+                author: '我被苍井空包养了',
+                avatar: 'http://wx.qlogo.cn/mmhead/Q3auHgzwzM76hwa8RNfuXy20DdryLojOIH46m7JIYfF8fZjIQouOpA/132',
+                content: '在这个世界上，人外有人，天外有天，不要以为自己有点本事，就目中无人，我们这里有一私人学校，舞蹈老师居然把体育老师的腿给打折了',
+                image: 'http://localhost:3000/images/1.jpg',
+                likes: 1431,
+                comments: 16,
+                shares: 4
+            }
+        ]
     },
 })
