81dccb58e5256bf93fb0b15e50653afdbb5c3c28
fov42550564 <42550564@qq.com>
2016-10-14 20:25:06 +0800

    添加获取列表

--- localServer/app.js
+++ localServer/app.js
@@ -1,66 +1,49 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var jsdom = require("jsdom");
+var jquery = require('fs').readFileSync('./utils/jquery.js').toString();
 var fs = require('fs');
-// var contentType = require('content-type');
-// var getRawBody = require('raw-body');
-// var qs = require('qs');
-
-// var des = require('./utils/des.js');
-var des = {
-    encode: (text, key)=>(text),
-    decode: (code, key)=>(code),
-};
-
-var deskey = "SV#Y!jAz";
 var app = express();
 
-app.TIMEOUT = 100;
-var modules = [
-    'main',
-];
-
 app.use(express.static(__dirname + '/public'));
-
-// app.use(function (req, res, next) {
-//     getRawBody(req, {
-//         length: req.headers['content-length'],
-//         limit: '200mb',
-//         encoding: contentType.parse(req).parameters.charset
-//     }, function (err, string) {
-//         if (err) return next(err)
-//         req.text = string
-//         req.body = qs.parse(req.text.toString());
-//         next()
-//     })
-// });
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(bodyParser.text());
 
-app.send = function(req, res, str) {
-    console.log("recv:", des.decode(req.body, deskey));
-    setTimeout(function() {
-        // console.log("send:", str);
-        res.send(des.encode(str, deskey));
-    }, app.TIMEOUT);
-};
-
-app.sendFile = function(req, res, filename) {
-    app.send(req, res, fs.readFileSync(filename, 'utf8'));
-};
-
-app.sendObj = function(req, res, obj) {
-    app.send(req, res, JSON.stringify(obj));
-};
-
-app.subPost = function(url, callback) {
-    app.post('/JFBSample/api'+url, callback);
-};
-
-for (var i in modules) {
-    require('./modules/'+modules[i]).register(app);
+function getContent(type, page, callback) {
+    jsdom.env({
+        url: 'http://www.qiushibaike.com/'+type+'/page/'+page,
+        headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:23.0) Gecko/20100101 Firefox/23.0' },
+        src: [jquery],
+        done: function (error, window) {
+            if (error) {
+                callback([]);
+                return;
+            }
+            var $ = window.$;
+            var list = [];
+            $("#content-left").find(".article").each(function() {
+                var item = {};
+                var authorImg = $(this).find('.author img');
+                item.avatar = authorImg.attr('src');
+                item.username = authorImg.attr('alt');
+                item.content =  $(this).find('.content span').html();
+                item.likes =  $(this).find('.stats-vote .number').html();
+                item.comments =  $(this).find('.qiushi_comments .number').html();
+                list.push(item);
+            });
+            callback(list);
+        }
+    });
 }
+
+app.post('/getList', function(req, res){
+    const {type, page} = req.body;
+    getContent(type, page, function(list) {
+        res.send({list: list});
+    });
+});
+
 
 app.listen(3000, function() {
     console.log("server listen on: http://localhost:3000");
