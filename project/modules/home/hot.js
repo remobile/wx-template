743b55d6b510f56ba4570e67338b4a5ebdcce0a8
fov42550564 <42550564@qq.com>
2016-10-15 01:20:55 +0800

    完成热门列表的显示

---
+++ project/modules/home/hot.js
@@ -0,0 +1,29 @@
+var app = getApp();
+Page({
+    data: {
+        list: [],
+        windowHeight: 0,
+        windowWidth: 0
+    },
+    onLoad() {
+        this.page = 0;
+        this.getList();
+    },
+    onShow: function(e) {
+        this.setData({
+            windowHeight: app.system.windowHeight,
+            windowWidth: app.system.windowWidth
+        })
+    },
+    pullUpLoad: function (e) {
+        this.getList();
+    },
+    getList() {
+        if (!this.data.loading) {
+            const param = {type: 'hot', page: ++this.page};
+            app.GET('http://localhost:3000/getList', param, (data)=>{
+                this.setData({list: this.data.list.concat(data.list)})
+            }, this);
+        }
+    },
+})
