8f29e267bc1dc7abf45ad0cdb1ad293356a1dcc3
fov42550564 <42550564@qq.com>
2016-10-15 22:09:36 +0800

    完成代码重用简化

---
+++ project/modules/home/components/list.js
@@ -0,0 +1,40 @@
+var app = getApp();
+module.exports = function(type) {
+    return {
+        data: {
+            list: [],
+            windowHeight: 0,
+            windowWidth: 0
+        },
+        onLoad() {
+            this.page = 0;
+            this.getList();
+        },
+        onShow: function(e) {
+            this.setData({
+                windowHeight: app.system.windowHeight,
+                windowWidth: app.system.windowWidth
+            })
+        },
+        pullUpLoad: function (e) {
+            this.getList();
+        },
+        onRowClick(e) {
+            var {index} = e.currentTarget.dataset;
+            app.navigator.push({
+                url: './detail',
+                passProps: {
+                    item: this.data.list[index],
+                }
+            });
+        },
+        getList() {
+            if (!this.data.loading) {
+                const param = {type, page: ++this.page};
+                app.GET('http://localhost:3000/getList', param, (list)=>{
+                    this.setData({list: this.data.list.concat(list)})
+                }, this);
+            }
+        },
+    };
+}
